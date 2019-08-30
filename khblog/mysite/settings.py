d16ba5104b1e675915b037663b1e3b1880b909b7
Khanze99 <38478252+Khanze99@users.noreply.github.com>
2019-08-30 19:04:05 +0300

    Update settings.py

--- khblog/mysite/settings.py
+++ khblog/mysite/settings.py
@@ -93,9 +93,9 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'khblog_db',
-        'USER': 'khanze',
-        'PASSWORD': 'ConeForest1928',
+        'NAME': '-',
+        'USER': '-',
+        'PASSWORD': '-',
         'HOST': '127.0.0.1',
         'PORT': 5432
     }
@@ -149,8 +149,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = "khamidovanvarkhnz@gmail.com"
-EMAIL_HOST_USER = "khamidovanvarkhnz@gmail.com"
-EMAIL_HOST_PASSWORD = "getRequestIndex1999"
+DEFAULT_FROM_EMAIL = "-"
+EMAIL_HOST_USER = "-"
+EMAIL_HOST_PASSWORD = "-"
 EMAIL_MESSAGE = "Hello my friend, in this blog you can share your news and learn new things from other registration. Thank you for being with us"
 
