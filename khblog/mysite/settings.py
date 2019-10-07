0f28e2acdd4db6186c01cd8113d257178d3ecec5
Khanze99 <khamidov_anvar@mail.ru>
2019-10-07 22:32:17 +0300

    update code

--- khblog/mysite/settings.py
+++ khblog/mysite/settings.py
@@ -147,9 +147,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-DEFAULT_FROM_EMAIL = "-"
-EMAIL_HOST_USER = "-"
-EMAIL_HOST_PASSWORD = "-"
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_MESSAGE = "Hello my friend, in this blog you can share your news and learn new things from other registration. Thank you for being with us"
 
 
