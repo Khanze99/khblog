f87460304ef4032d863ffb8060ce7252590839f2
Khanze99 <khamidov_anvar@mail.ru>
2020-05-01 21:47:30 +0300

    update my and liked posts

--- khblog/accounts/image.py
+++
@@ -1,9 +0,0 @@
-# from PIL import Image
-#
-# # image = Image.open('media/profile_pics/NAvccyrG7kE.jpg')
-# image = Image.open('media/default.png')
-# size = image.size
-# print(size[0])
-# res = image.resize((int(size[0]/2), int(size[1]/2)), Image.ANTIALIAS)
-# res.save('media/default.png')
-# res.show()
